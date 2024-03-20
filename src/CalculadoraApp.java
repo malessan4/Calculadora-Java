392f106535b29801fe14526997f7ac5112e455c6
Matias-Alessandrello <matialess@gmail.com>
2024-03-20 08:34:25 -0300

    finalizamos la construccion de una calculadora APP

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -2,24 +2,72 @@ import java.util.Scanner;
 
 public class CalculadoraApp {
     public static void main(String[] args) {
-        System.out.println("****Aplicación Calculadora****");
-        //Mostramoes el menu
-        //System.out.println("1. Suma \n2. Resta \n3.Multiplicacion");
-        System.out.println("""
-                1. Suma
-                2. Resta
-                3. Multiplicacion
-                4. Division
-                5. Salir
-                """);
-
-        System.out.print("Proporciona valor operando1: ");
         Scanner consola = new Scanner(System.in);
-        var operando1 = Integer.parseInt(consola.nextLine());
-        System.out.print("Proporciona valor operando2: ");
-        var operando2 = Integer.parseInt(consola.nextLine());
-        var resultado = operando1 + operando2;
-        System.out.println("Resultado: " + resultado);
+        while (true) {
 
+            System.out.println("****Aplicación Calculadora****");
+            //Mostramoes el menu
+            mostrarMenu();
+
+            try {
+                var operacion = Integer.parseInt(consola.nextLine());
+                //Revisar que este dentro de las opciones mencionadas
+                if (operacion >= 1 && operacion <= 4) {
+                    //Ejecutamos la operacion deseada
+                    ejecutarOperacion(operacion, consola);
+                } else if (operacion == 5) { // Salir
+                    System.out.println("Hasta pronto...");
+                    break;
+                } else {
+                    System.out.println("Opcion erronea: " + operacion);
+                    }
+                // imprimimos un salto antes de repetir el ciclo
+                System.out.println();
+            } //fin try
+            catch (Exception e) {
+                System.out.println("Ocurrio un error: " + e.getMessage());
+                System.out.println();
+            }
+        } //fin while
+    } // fin main
+
+    private static void mostrarMenu() {
+        //System.out.println("1. Suma \n2. Resta \n3.Multiplicacion"); otra forma de escribirlo
+        System.out.println("""
+                    1. Suma
+                    2. Resta
+                    3. Multiplicacion
+                    4. Division
+                    5. Salir
+                    """);
+        System.out.print("Operacion a realizar? ");
     }
-}
+
+    private static void ejecutarOperacion(int operacion, Scanner consola){
+        System.out.print("Proporciona valor operando1: ");
+        var operando1 = Double.parseDouble(consola.nextLine());
+        System.out.print("Proporciona valor operando2: ");
+        var operando2 = Double.parseDouble(consola.nextLine());
+        double resultado;
+        switch (operacion) {
+            case 1 -> { // Suma
+                resultado = operando1 + operando2;
+                System.out.println("Resultado Suma: " + resultado);
+            }
+            case 2 -> { // Resta
+                resultado = operando1 - operando2;
+                System.out.println("Resultado Resta: " + resultado);
+            }
+            case 3 -> { // Multiplicacion
+                resultado = operando1 * operando2;
+                System.out.println("Resultado Multiplicacion: " + resultado);
+            }
+            case 4 -> { // Division
+                resultado = operando1 / operando2;
+                System.out.println("Resultado Division: " + resultado);
+            }
+            default -> System.out.println("Opcion erronea: " + operacion);
+        }
+    }
+
+} // fin clase
